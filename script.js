105eed3297074de2095a35f8f986e5f7130ad339
Francisco Schwindt <fran2002.sch@gmail.com>
2023-06-28 23:25:04 -0300

    ahora anda el rainbow y el boton pausa el efecto y cambia el color

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Obtener el elemento con el id "boton"
 var boton = document.getElementById("boton");
 
+// Obtener el elemento con el id "header"
+var header = document.querySelector("header");
+
+// Variable para controlar el estado del efecto de arco iris
+var rainbowActive = true;
+
 // Agregar un evento de clic al botón
 boton.addEventListener("click", function() {
-  // Cambiar el color de fondo del encabezado al hacer clic en el botón
-  var header = document.querySelector("header");
-  header.style.backgroundColor = getRandomColor();
+  // Cambiar el estado del efecto de arco iris
+  rainbowActive = !rainbowActive;
+  
+  // Detener o reanudar la animación de arco iris
+  if (rainbowActive) {
+    header.style.animationPlayState = "running";
+  } else {
+    header.style.animationPlayState = "paused";
+    header.style.backgroundColor = getRandomColor();
+  }
 });
 
 // Función para generar un color aleatorio en formato hexadecimal
