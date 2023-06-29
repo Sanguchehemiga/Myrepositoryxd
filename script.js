da2a680d30145a7e8edb4e4ae23fda6d62cc0d34
Francisco Schwindt <fran2002.sch@gmail.com>
2023-06-28 23:17:27 -0300

    ahora anda el boton para cambiar color, pero no tengo arcoiris

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ var boton = document.getElementById("boton");
 
 // Agregar un evento de clic al botón
 boton.addEventListener("click", function() {
-  alert("¡Has hecho clic en el botón!");
+  // Cambiar el color de fondo del encabezado al hacer clic en el botón
+  var header = document.querySelector("header");
+  header.style.backgroundColor = getRandomColor();
 });
+
+// Función para generar un color aleatorio en formato hexadecimal
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
